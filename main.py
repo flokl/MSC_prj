8d07683db51633304252f247600cc59863086ff2
Florian Klotz <if18b162@technikum-wien.at>
2022-03-28 12:22:56 +0200

    Add document read and write

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ PHISHINGMAIL_ENTRY_FILE = "phishingmail_entries.json"
 def main():
     pass
 
+def read_txt(filename):
+    data = []
+    with open(filename, 'r') as input_file:
+        data.append(input_file.readline())
+    return data
+
+
+def write_csv(filename, data):
+    with open(filename, 'w') as output_file:
+        for line in data:
+            output_file.write(';'.join(line))
+            output_file.write('\n')
 
 if __name__ == "__main__":
     main()
