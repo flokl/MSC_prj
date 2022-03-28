e8010001512c6b6aeeac30027fc15615817a65e7
Florian Klotz <if18b162@technikum-wien.at>
2022-03-28 12:21:26 +0200

    Init

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+OUTPUT_FILE = "scenario_data.csv"
+TRASH_ENTRY_FILE = "trash_entries.txt"
+PHISHINGMAIL_ENTRY_FILE = "phishingmail_entries.json"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
