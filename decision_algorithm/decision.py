3793f434bf54300998c17dd59b445e927dea2a0b
johanneskogoj <j.kogoj@me.com>
2022-06-04 10:23:07 +0200

    ordnerstruktur

---
+++ decision_algorithm/decision.py
@@ -0,0 +1,9 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+
