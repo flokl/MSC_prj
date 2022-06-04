49e0f7f1256308328e5a0afeab63700407d898d6
johanneskogoj <j.kogoj@me.com>
2022-06-04 13:52:52 +0200

    filter relevant items from csv

--- decision_algorithm/decision.py
+++ decision_algorithm/decision.py
@@ -1,9 +1,26 @@
-import numpy as np
-from matplotlib import pyplot as plt
 
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
+import csv
 
+def readCSV(stringOfCSV): # reads CSV and Puts every line in a list and returns list of list
+    with open(stringOfCSV, newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    return data
 
+def getRelevantItemsFromList(data): #takes all items and returns the relevant ones
+    allElementsWithHierarchy = list()
+    for i in data:
+        allElementsWithHierarchy.append(i[0].split(';'))
+    allElements = [item for sublist in allElementsWithHierarchy for item in sublist]
+    singleElements = list(dict.fromkeys(allElements))
+    relevantItems = list()
+    for i in singleElements:
+        count = allElements.count(i)
+        if count >= len(allElementsWithHierarchy) / 10:
+            relevantItems.append(i)
+    return relevantItems
+
+csvPath='../data_gen/scenario_data.csv'
+dataAsListofList=readCSV(csvPath)
+relevantItems=getRelevantItemsFromList(dataAsListofList)
+print(relevantItems)
